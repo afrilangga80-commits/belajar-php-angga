8779c050062069eace8234ebd9abb3d5e7dee72d
Angga <afrilangga80@gmail.com>
2025-09-04 02:02:46 -0700

    menambah file Hello.php dan Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nama: Afril Angga Kurniawan\n";
+echo "Umur: 17 tahun\n";
+echo "Sekolah: SMK.P\n";
+echo "Jurusan: RPL/Xll\n";
+?>
---
+++ Hello.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hello, World! \n";
+echo "Hello, World! \n";
+
+// ini komentar
+/*
+ini komentar
+2 baris
+*/

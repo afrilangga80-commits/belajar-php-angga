dfa5b27b8de2a52271fd456910aa64c13013c50f
Angga <afrilangga80@gmail.com>
2025-09-07 20:50:02 -0700

    belajar php variabel dan tipe data

---
+++ Variabel.php
@@ -0,0 +1,17 @@
+<?php
+$nama_lengkap = "Afril Angga Kurniawan";
+$umur = 15;
+$alamat = "Pacitan";
+$sudah_lulus = true;
+
+// Tampilkan dengan echo
+echo "Nama : $Afril Angga Kurniawan\n";
+echo "Umur : $15\n";
+echo "Alamat : $Pacitan\n";
+echo "Sudah Lulus : $true\n";
+
+//Tampilkan dengan var_dump
+var_dump($Afril Angga Kurniawan);
+var_dump(15);
+var_dump(Pacitan);
+var_dump(true);
